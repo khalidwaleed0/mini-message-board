42a0ffba74c7fbf07f3d9395b8a79cb12f1e5f06
Khalid Waleed <khalidwaleed0@outlook.com>
2023-11-26 18:40:27 +0200

    Added mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date().toLocaleString(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date().toLocaleString(),
-  },
-];
+mongoose.connect("mongodb://127.0.0.1/mini-messages-board-db");
+const messageSchema = new mongoose.Schema({
+  text: String,
+  user: String,
+  added: Date,
+});
 
-router.get("/", function (req, res) {
-  res.render("index", { title: "Mini Message Board", messages: messages });
+const messages = mongoose.model("messages", messageSchema);
+
+router.get("/", async function (req, res) {
+  res.render("index", { title: "Mini Message Board", messages: await messages.find({}) });
 });
 
 router.get("/new", function (req, res) {
   res.render("form");
 });
 
-router.post("/new", function (req, res) {
-  messages.push({ user: req.body.user, text: req.body.message, added: new Date().toLocaleString() });
-  res.redirect("/");
+router.post("/new", async function (req, res) {
+  try {
+    await messages.create({
+      user: req.body.user,
+      text: req.body.message,
+      added: new Date(),
+    });
+    res.redirect("/");
+  } catch (e) {
+    console.log(e.message);
+  }
 });
 
 module.exports = router;
